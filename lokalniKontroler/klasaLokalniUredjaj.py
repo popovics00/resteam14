a1df42950cffb08fc13dea205e1eceef00ac50c8
popovics00 <93131229+popovics00@users.noreply.github.com>
2022-06-19 13:20:34 +0200

    Sredjivanje koda za testiranje i gasenje programa

--- lokalniKontroler/klasaLokalniUredjaj.py
+++ lokalniKontroler/klasaLokalniUredjaj.py
@@ -1,14 +1,14 @@
 class lokalniUredjaj:
-    def __init__(self, imeUredjaja, vreme, trenutnaVrednost):
+    def __init__(self, imeUredjaja, vreme, trenutnaVrednost):#pragma:no cover
         self.imeUredjaja = imeUredjaja
         self.vreme = vreme
         self.trenutnaVrednost = trenutnaVrednost
 
-class vremeSlanja:
-    def __init__(self, vremeSlanja):
+class vremeSlanja: 
+    def __init__(self, vremeSlanja): #pragma:no cover
         self.vremeSlanja = vremeSlanja
 
 class Kontroler:
-    def __init__(self, imeKontrolera, portKontrolera):
+    def __init__(self, imeKontrolera, portKontrolera):#pragma:no cover
         self.imeKontrolera = imeKontrolera
         self.portKontrolera = portKontrolera
--- lokalniKontroler/vremeFunction.py
+++ lokalniKontroler/vremeFunction.py
@@ -1,8 +1,16 @@
 import xml.etree.ElementTree as ET
 
-def PreuzmiVreme():
+#testirano
+def toInt(broj):
+    try:
+        return int(broj)
+    except:
+        return "ERROR"
+
+#nema potrebe testirati jer sve sto moze gresku napraviti je toInt a to je testirano
+def PreuzmiVreme(): #pragma:no cover
     tempString = "C:\\Users\\stefa\\Desktop\\RES_Projekat\\Projekat2\\resteam14\\lokalniKontroler\\vremeSlanja.xml"
     tree = ET.parse(tempString) 
     root = tree.getroot() 
-    povratak = int(root[0].text)
+    povratak = toInt(root[0].text)
     return povratak
